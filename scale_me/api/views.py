d43a8365aa751ef236cfc11377a1d623b665ba49
Ryan Coughlin <rlc4sv@virginia.edu>
2017-09-14 11:26:20 -0400

    created sample models, working json response

--- scale_me/api/views.py
+++ scale_me/api/views.py
@@ -1,5 +1,12 @@
 from django.http import HttpResponse
+from django.http import JsonResponse
+
+from .models import Question
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the API index.")
+
+def detail(request, question_id):
+    my_question = Question.objects.get(pk=question_id)
+    return JsonResponse({'text':my_question.question_text})
